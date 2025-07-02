7e4ecaf9f3fd48f61cdddfab73224d8167c4846e
antorwap <antor01717@gmail.com>
2025-07-02 14:56:28 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyATseaFdrJ6JvDCyxMrB98q3xm1R3KW0Ao",
+  authDomain: "antorerp.firebaseapp.com",
+  projectId: "antorerp",
+  storageBucket: "antorerp.appspot.com",
+  messagingSenderId: "348094724361",
+  appId: "1:348094724361:web:0e71de2ba92145d2037bd8",
+  measurementId: "G-367MZ8RMBZ"
+};
+firebase.initializeApp(firebaseConfig);
