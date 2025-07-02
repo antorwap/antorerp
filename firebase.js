98af5c1ea8d3c72ea75bc76ba9f748d606762dbc
antorwap <antor01717@gmail.com>
2025-07-02 15:23:43 +0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
-// Firebase config
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATseaFdrJ6JvDCyxMrB98q3xm1R3KW0Ao",
+  authDomain: "antorerp.firebaseapp.com",
+  projectId: "antorerp",
+  storageBucket: "antorerp.appspot.com",
+  messagingSenderId: "348094724361",
+  appId: "1:348094724361:web:0e71de2ba92145d2037bd8",
+  measurementId: "G-367MZ8RMBZ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
