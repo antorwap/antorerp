615e64ebd75f4a4555d1f811f38e89cfaaa6ced4
antorwap <antor01717@gmail.com>
2025-07-02 15:15:57 +0600

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,16 +1,1 @@
-
-import { auth } from './firebase.js';
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-window.login = () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "dashboard.html";
-    })
-    .catch((error) => {
-      alert("Login failed: " + error.message);
-    });
-};
+// login/signup logic here
