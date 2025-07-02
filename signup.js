d32c85d7467c4de2c460b8b70f133a36ca222a80
antorwap <antor01717@gmail.com>
2025-07-02 16:57:25 +0600

    Delete signup.js

--- signup.js
+++
@@ -1,7 +0,0 @@
-function signup() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(() => { window.location.href = "dashboard.html"; })
-    .catch((error) => { alert(error.message); });
-}
